a3a5d66c95e18b475fc98ea092dfdd5dc92bbf64
Pallav <pallav.pokhriyal.01@gmail.com>
2018-01-05 23:01:07 +1100

    Initial

---
+++ Gene/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gene.Startup))]
+namespace Gene
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
